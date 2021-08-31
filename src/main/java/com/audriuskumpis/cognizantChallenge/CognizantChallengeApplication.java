8295a5ada44de2988037c37a7f97884f57c819f8
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2021-08-31 11:59:55 +0300

    read json to mongo database

--- src/main/java/com/audriuskumpis/cognizantChallenge/CognizantChallengeApplication.java
+++ src/main/java/com/audriuskumpis/cognizantChallenge/CognizantChallengeApplication.java
@@ -1,7 +1,17 @@
 package com.audriuskumpis.cognizantChallenge;
 
+import com.audriuskumpis.cognizantChallenge.entity.Warehouse;
+import com.audriuskumpis.cognizantChallenge.service.WarehouseService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @SpringBootApplication
 public class CognizantChallengeApplication {
@@ -10,4 +20,20 @@ public class CognizantChallengeApplication {
 		SpringApplication.run(CognizantChallengeApplication.class, args);
 	}
 
+	// imports data from given .json file to repository on application start
+	@Bean
+	CommandLineRunner runner(WarehouseService warehouseService) {
+		return args -> {
+			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<Warehouse>> typeReference = new TypeReference<>() {};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/warehouses.json");
+			try {
+				List<Warehouse> warehouses = mapper.readValue(inputStream, typeReference);
+				warehouseService.save(warehouses);
+				System.out.println("Warehouses saved!");
+			} catch (IOException e) {
+				System.err.println("Unable to load warehouses: " + e.getMessage());
+			}
+		};
+	}
 }
