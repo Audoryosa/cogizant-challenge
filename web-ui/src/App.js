8d3dc234ae50d3689ee860231f2c2e839266c201
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2021-08-31 13:13:19 +0300

    front end get request test

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+    state = {
+        cars: []
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:8081/api/v1/warehouse/vehicle`)
+            .then(res => {
+                const cars = res.data;
+                this.setState({ cars });
+            })
+    }
+
+    render() {
+        return (
+            <ul>
+                { this.state.cars.map(car => <li>{car.make} {car.model}</li>)}
+            </ul>
+        )
+    }
 }
-
-export default App;
