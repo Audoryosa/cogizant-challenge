65b82e6d743cc63b0dc5306f7de3fd20e14554dc
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2021-08-31 11:43:34 +0300

    base files

---
+++ src/main/java/com/audriuskumpis/cognizantChallenge/CognizantChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.audriuskumpis.cognizantChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognizantChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognizantChallengeApplication.class, args);
+	}
+
+}
