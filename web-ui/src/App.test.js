4985b71d06fbd6fbbbd8f37efa4ea5a407221673
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2021-09-02 10:58:32 +0300

    Generic front end test updated

--- web-ui/src/App.test.js
+++ web-ui/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />);
   const linkElement = screen.getByText(/Available second hand vehicles, all in one place!/i);
   expect(linkElement).toBeInTheDocument();
